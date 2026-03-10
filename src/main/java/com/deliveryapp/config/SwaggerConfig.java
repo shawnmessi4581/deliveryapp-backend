89a6d2d34e3eaaa32ef63467ef2daf8da7661486
shawnmessi4581 <shawnmessi4581@gmail.com>
2026-03-10 16:18:35 +0300

    the new host

--- src/main/java/com/deliveryapp/config/SwaggerConfig.java
+++ src/main/java/com/deliveryapp/config/SwaggerConfig.java
@@ -6,32 +6,36 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        final String securitySchemeName = "bearerAuth";
+        @Bean
+        public OpenAPI openAPI() {
+                final String securitySchemeName = "bearerAuth";
 
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Delivery App API")
-                        .description("REST API documentation for the Delivery App")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Your Name")
-                                .email("your@email.com")))
-                // 👇 This adds the JWT "Authorize" button in Swagger UI
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
+                return new OpenAPI()
+                                .servers(List.of(
+                                                new Server().url("https://allin-shops.com"),
+                                                new Server().url("http://localhost:8080")))
+                                .info(new Info()
+                                                .title("Allin API")
+                                                .description("REST API documentation forAllin")
+                                                .version("1.0.0")
+                                                .contact(new Contact()
+                                                                .name("ENG.Mohahamad Allaith Mojadamy")
+                                                                .email("mohamadallays.mg@email.com")))
+                                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                                .components(new Components()
+                                                .addSecuritySchemes(securitySchemeName,
+                                                                new SecurityScheme()
+                                                                                .name(securitySchemeName)
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")));
+        }
 }
