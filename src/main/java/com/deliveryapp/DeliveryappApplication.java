2d3f11c5a73e2ad16e17f590408f61c7e50a1c7b
shawnmessi4581 <shawnmessi4581@gmail.com>
2026-04-01 03:27:44 -0700

    feat: configure application and database timezone to Asia/Damascus

--- src/main/java/com/deliveryapp/DeliveryappApplication.java
+++ src/main/java/com/deliveryapp/DeliveryappApplication.java
@@ -1,15 +1,23 @@
 package com.deliveryapp;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
-@EnableAsync // Add this!
+@EnableAsync
 public class DeliveryappApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Damascus"));
+        System.out.println("🌍 Spring Boot Application TimeZone set to: " + TimeZone.getDefault().getID());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DeliveryappApplication.class, args);
     }
-
 }
